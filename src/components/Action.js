b187e9243c2bb6377d088fd9b6c980f3b40e2757
Stackbit Code Editor <projects@stackbit.com>
2021-09-29 22:30:52 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -27,7 +27,7 @@ export default class Action extends React.Component {
         }
 
         return (
-            <Link href={withPrefix(url)} {...attrs} className={classes}>
+            <Link href={(url)} {...attrs} className={classes}>
                 {(style === 'icon') ? (
                     <React.Fragment>
                         <Icon icon={icon} />
